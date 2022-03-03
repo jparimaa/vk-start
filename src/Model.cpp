51f763d5662b03125bf985ea2f9d5de9bc14f6e8
JP Arimaa <juhapekka.arimaa@gmail.com>
2022-03-03 21:29:53 +0200

    Load images and materials

--- src/Model.cpp
+++ src/Model.cpp
@@ -35,34 +35,10 @@ size_t getAccessorElementSizeInBytes(const tinygltf::Accessor& accessor)
     const size_t typeCount = c_typeCounts.at(accessor.type);
     return componentTypeSize * typeCount;
 }
-} // namespace
 
-Model::Model(const std::string& filename)
+std::vector<Model::Vertex> loadVertices(const tinygltf::Model& model)
 {
-    tinygltf::Model model;
-    tinygltf::TinyGLTF loader;
-    std::string errorMessage;
-    std::string warningMessage;
-
-    const std::string filepath = c_modelsFolder + filename;
-    printf("Loading model %s... ", filepath.c_str());
-    const bool modelLoaded = loader.LoadBinaryFromFile(&model, &errorMessage, &warningMessage, filepath);
-
-    if (!warningMessage.empty())
-    {
-        LOGW(warningMessage.c_str());
-        abort();
-    }
-
-    if (!errorMessage.empty())
-    {
-        LOGE(errorMessage.c_str());
-        abort();
-    }
-
-    CHECK(modelLoaded);
-    CHECK(!model.meshes.empty());
-
+    std::vector<Model::Vertex> vertices;
     for (const auto& [attributeName, attributeIndex] : model.meshes[0].primitives[0].attributes)
     {
         const tinygltf::Accessor& accessor = model.accessors[attributeIndex];
@@ -96,6 +72,12 @@ Model::Model(const std::string& filename)
             bufferPtr += elementSizeInBytes + bufferView.byteStride;
         }
     }
+    return vertices;
+}
+
+std::vector<uint32_t> loadIndices(const tinygltf::Model& model)
+{
+    std::vector<uint32_t> indices;
 
     const tinygltf::Accessor& indicesAccessor = model.accessors[model.meshes[0].primitives[0].indices];
     const tinygltf::BufferView& indexBufferView = model.bufferViews[indicesAccessor.bufferView];
@@ -113,9 +95,72 @@ Model::Model(const std::string& filename)
         indexBufferPtr += indexBufferView.byteStride + sizeof(unsigned short);
     }
 
-    printf("Completed\n");
+    return indices;
 }
 
-Model::~Model()
+std::vector<Model::Material> loadMaterials(const tinygltf::Model& model)
 {
+    std::vector<Model::Material> materials(model.materials.size());
+
+    for (size_t i = 0; i < model.materials.size(); ++i)
+    {
+        const tinygltf::Material& m = model.materials[i];
+        materials[i].baseColor = model.textures[m.pbrMetallicRoughness.baseColorTexture.index].source;
+        materials[i].metallicRoughnessImage = model.textures[m.pbrMetallicRoughness.metallicRoughnessTexture.index].source;
+        materials[i].normalImage = model.textures[m.normalTexture.index].source;
+        materials[i].emissiveImage = model.textures[m.emissiveTexture.index].source;
+        materials[i].occlusionImage = model.textures[m.occlusionTexture.index].source;
+    }
+
+    return materials;
+}
+
+std::vector<Model::Image> loadImages(tinygltf::Model& model)
+{
+    std::vector<Model::Image> images(model.images.size());
+
+    for (size_t i = 0; i < model.images.size(); ++i)
+    {
+        images[i].width = model.images[i].width;
+        images[i].height = model.images[i].height;
+        images[i].components = model.images[i].component;
+        images[i].bitsPerChannel = model.images[i].bits;
+        images[i].data = std::move(model.images[i].image);
+    }
+    return images;
+}
+} // namespace
+
+Model::Model(const std::string& filename)
+{
+    tinygltf::Model model;
+    tinygltf::TinyGLTF loader;
+    std::string errorMessage;
+    std::string warningMessage;
+
+    const std::string filepath = c_modelsFolder + filename;
+    printf("Loading model %s... ", filepath.c_str());
+    const bool modelLoaded = loader.LoadBinaryFromFile(&model, &errorMessage, &warningMessage, filepath);
+
+    if (!warningMessage.empty())
+    {
+        LOGW(warningMessage.c_str());
+        abort();
+    }
+
+    if (!errorMessage.empty())
+    {
+        LOGE(errorMessage.c_str());
+        abort();
+    }
+
+    CHECK(modelLoaded);
+    CHECK(!model.meshes.empty());
+
+    vertices = loadVertices(model);
+    indices = loadIndices(model);
+    materials = loadMaterials(model);
+    images = loadImages(model);
+
+    printf("Completed\n");
 }
--- src/Model.hpp
+++ src/Model.hpp
@@ -14,11 +14,30 @@ public:
         glm::vec3 normal;
     };
 
+    struct Material
+    {
+        int baseColor;
+        int metallicRoughnessImage;
+        int normalImage;
+        int emissiveImage;
+        int occlusionImage;
+    };
+
+    struct Image
+    {
+        int width;
+        int height;
+        int components;
+        int bitsPerChannel;
+        std::vector<unsigned char> data;
+    };
+
     using Index = uint32_t;
 
     Model(const std::string& filename);
-    ~Model();
 
     std::vector<Vertex> vertices;
     std::vector<uint32_t> indices;
+    std::vector<Material> materials;
+    std::vector<Image> images;
 };
